d441e2901a8ca8f785ed65f730fae4f8095f6533
Nama Anda <email@example.com>
2024-07-22 07:03:32 +0700

    Menambahkan edit.php

---
+++ app/Views/Users/edit.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Edit User</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <h1>Edit User</h1>
+    <form action="/users/update/<?= $user['id'] ?>" method="post">
+        <label>Name</label>
+        <input type="text" name="name" value="<?= $user['name'] ?>" required><br>
+        <label>Email</label>
+        <input type="email" name="email" value="<?= $user['email'] ?>" required><br>
+        <label>Password</label>
+        <input type="password" name="password" required><br>
+        <button type="submit">Update</button>
+    </form>
+</body>
+</html>
